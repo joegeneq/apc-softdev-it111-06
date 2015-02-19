9e76b18d7ebdea6a1875c35045a1edce46589bbc
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-02-19 15:37:33 +0800

    Cleaned codes (removed commas and apostrophes)

--- application/basic/views/site/about.php
+++ application/basic/views/site/about.php
@@ -7,11 +7,11 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
    <center>
-   '<a href="index.php?r=company"><img src="images/Companies.png"  /></a>';
-   '<a href="index.php?r=customer"><img src="images/Customers.png"  /></a>';
-   '<a href="index.php?r=item"><img src="images/Items.png"  /></a>';
-   '<a href="index.php?r=order"><img src="images/Orders.png"  /></a>';
-   '<a href="index.php?r=sale"><img src="images/Sales.png"  /></a>';
-   '<a href="index.php?r=supplier"><img src="images/Suppliers.png"  /></a>';
+   <a href="index.php?r=company"><img src="images/Companies.png"  /></a>
+   <a href="index.php?r=customer"><img src="images/Customers.png"  /></a>
+   <a href="index.php?r=item"><img src="images/Items.png"  /></a>
+   <a href="index.php?r=order"><img src="images/Orders.png"  /></a>
+   <a href="index.php?r=sale"><img src="images/Sales.png"  /></a>
+   <a href="index.php?r=supplier"><img src="images/Suppliers.png"  /></a>
      </center>
 </div>
