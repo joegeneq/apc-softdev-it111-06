c8613fa391f6637dc1243995c9291e79abd89438
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-02-20 23:08:27 +0800

    Added models, CRUD and db (Region, Province, City) - it111_exercise1

---
+++ practice/aideocampo/basic/models/City.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property integer $id
+ * @property string $city_code
+ * @property string $city_description
+ * @property integer $province_id
+ *
+ * @property Province $province
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['province_id'], 'required'],
+            [['province_id'], 'integer'],
+            [['city_code', 'city_description'], 'string', 'max' => 32]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'city_code' => 'City Code',
+            'city_description' => 'City Description',
+            'province_id' => 'Province ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProvince()
+    {
+        return $this->hasOne(Province::className(), ['id' => 'province_id']);
+    }
+}
---
+++ practice/aideocampo/database/it111_exercise1.sql
@@ -0,0 +1,66 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema it111_exercise1
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema it111_exercise1
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `it111_exercise1` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `it111_exercise1` ;
+
+-- -----------------------------------------------------
+-- Table `it111_exercise1`.`region`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `it111_exercise1`.`region` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `region_code` VARCHAR(32) NULL,
+  `region_description` VARCHAR(32) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `it111_exercise1`.`province`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `it111_exercise1`.`province` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `province_code` VARCHAR(32) NULL,
+  `province_description` VARCHAR(32) NULL,
+  `region_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `region_id`),
+  INDEX `fk_province_region1_idx` (`region_id` ASC),
+  CONSTRAINT `fk_province_region1`
+    FOREIGN KEY (`region_id`)
+    REFERENCES `it111_exercise1`.`region` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `it111_exercise1`.`city`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `it111_exercise1`.`city` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `city_code` VARCHAR(32) NULL,
+  `city_description` VARCHAR(32) NULL,
+  `province_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `province_id`),
+  INDEX `fk_city_province_idx` (`province_id` ASC),
+  CONSTRAINT `fk_city_province`
+    FOREIGN KEY (`province_id`)
+    REFERENCES `it111_exercise1`.`province` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
