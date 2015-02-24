f0b82d5763c257db27086db60d9e84b26f9fa057
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-02-24 16:16:43 +0800

    Updated about.php

--- application/basic/views/site/about.php
+++ application/basic/views/site/about.php
@@ -6,12 +6,12 @@ $this->title = 'Inventory System';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-   <center>
-   <a href="index.php?r=company"><img src="images/Companies.png" onmouseover="this.src='images/Companies1.png'" onmouseout="this.src='images/Companies.png'"></a>
-   <a href="index.php?r=customer"><img src="images/Customers.png" onmouseover="this.src='images/Customers1.png'" onmouseout="this.src='images/Customers.png'"></a>
-   <a href="index.php?r=item"><img src="images/Items.png" onmouseover="this.src='images/Items1.png'" onmouseout="this.src='images/Items.png'"></a>
-   <a href="index.php?r=order"><img src="images/Orders.png" onmouseover="this.src='images/Orders1.png'" onmouseout="this.src='images/Orders.png'" ></a>
-   <a href="index.php?r=sale"><img src="images/Sales.png" onmouseover="this.src='images/Sales1.png'" onmouseout="this.src='images/Sales.png'"></a>
-   <a href="index.php?r=supplier"><img src="images/Suppliers.png" onmouseover="this.src='images/Suppliers1.png'" onmouseout="this.src='images/Suppliers.png'"></a>
+   <center> 
+   <a href="index.php?r=company"><img src="images/Companies.png" onmouseover="this.src='images/Companies1.png'" onmouseout="this.src='images/Companies.png'" alt="Companies"></a>
+   <a href="index.php?r=customer"><img src="images/Customers.png" onmouseover="this.src='images/Customers1.png'" onmouseout="this.src='images/Customers.png'" alt="Customers"></a>
+   <a href="index.php?r=item"><img src="images/Items.png" onmouseover="this.src='images/Items1.png'" onmouseout="this.src='images/Items.png'" alt="Items"></a>
+   <a href="index.php?r=order"><img src="images/Orders.png" onmouseover="this.src='images/Orders1.png'" onmouseout="this.src='images/Orders.png'" alt="Orders"></a>
+   <a href="index.php?r=sale"><img src="images/Sales.png" onmouseover="this.src='images/Sales1.png'" onmouseout="this.src='images/Sales.png'" alt="Sales"></a>
+   <a href="index.php?r=supplier"><img src="images/Suppliers.png" onmouseover="this.src='images/Suppliers1.png'" onmouseout="this.src='images/Suppliers.png'" alt="Suppliers"></a>
      </center>
 </div>
