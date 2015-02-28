f15ff5bc45bce63e38f475a8f455973451291990
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-02-28 10:40:53 +0800

    Changed the database connection of ourspace/config/db.php

--- practice/midterm_test/DeOcampo_Pauline_Gabrielle_midterms/ourspace/config/db.php
+++ practice/midterm_test/DeOcampo_Pauline_Gabrielle_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_deocampo_paulinegabrielle_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
