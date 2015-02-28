8493362660ab258f883c1cbb3d60441a73e28515
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-02-28 11:24:31 +0800

    Added record for practice/midterm_test/DeOcampo_Pauline_Gabrielle_midterms/softdev_deocampo_paulinegabrielle_ourspace.sql

--- practice/midterm_test/DeOcampo_Pauline_Gabrielle_midterms/softdev_deocampo_paulinegabrielle_ourspace.sql
+++ practice/midterm_test/DeOcampo_Pauline_Gabrielle_midterms/softdev_deocampo_paulinegabrielle_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 04:13 AM
+-- Generation Time: Feb 28, 2015 at 04:23 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -37,7 +37,14 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `landline` varchar(20) NOT NULL,
   `cellphone` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1, 'Pauline', 'Ignacio', 'De Ocampo', 'F', '2015-02-28 03:21:54', 'Makati City', '751-1234', '09123456789');
 
 -- --------------------------------------------------------
 
@@ -53,7 +60,14 @@ CREATE TABLE IF NOT EXISTS `mycomment` (
   `myaddress_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `mycomment_ibfk_1` (`myaddress_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `author`, `body`, `created_at`, `myaddress_id`) VALUES
+(1, 'Pauline De Ocampo', 'Hello test', '2015-02-28 03:22:40', 1);
 
 --
 -- Constraints for dumped tables
