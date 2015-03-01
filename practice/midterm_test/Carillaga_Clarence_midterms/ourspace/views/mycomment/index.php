0e9c2710718f1a5c0de3fe8198a571a7ecb119a1
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-03-01 19:50:41 +0800

    Updated index.php and view.php changed foreign key to last name

--- practice/midterm_test/Carillaga_Clarence_midterms/ourspace/views/mycomment/index.php
+++ practice/midterm_test/Carillaga_Clarence_midterms/ourspace/views/mycomment/index.php
@@ -1,38 +1,38 @@
 <?php
-
-use yii\helpers\Html;
-use yii\grid\GridView;
-
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\MycommentSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Mycomments';
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="mycomment-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Mycomment', ['create'], ['class' => 'btn btn-success']) ?>
+	
+	use yii\helpers\Html;
+	use yii\grid\GridView;
+	
+	/* @var $this yii\web\View */
+	/* @var $searchModel app\models\MycommentSearch */
+	/* @var $dataProvider yii\data\ActiveDataProvider */
+	
+	$this->title = 'Mycomments';
+	$this->params['breadcrumbs'][] = $this->title;
+	?>
+	<div class="mycomment-index">
+	
+	    <h1><?= Html::encode($this->title) ?></h1>
+	    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+	
+	    <p>
+	        <?= Html::a('Create Mycomment', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
+			'dataProvider' => $dataProvider,
+			'filterModel' => $searchModel,
+			'columns' => [
+			['class' => 'yii\grid\SerialColumn'],
+	
             'id',
-            'myaddress_id',
-            'author',
+            'myaddress.lastname',
+           'author',
             'body:ntext',
             'created_at',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+   ]); ?>
 
 </div>
--- practice/midterm_test/Carillaga_Clarence_midterms/ourspace/views/mycomment/view.php
+++ practice/midterm_test/Carillaga_Clarence_midterms/ourspace/views/mycomment/view.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'model' => $model,
         'attributes' => [
             'id',
-            'myaddress_id',
+            'myaddress.lastname',
             'author',
             'body:ntext',
             'created_at',
