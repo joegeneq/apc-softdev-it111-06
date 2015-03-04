0b28b0faffb8aa8950d869d26696f28110e46308
Phil Harvey Burgos <phburgoss@gmail.com>
2015-03-04 15:19:16 +0800

    Installed yii2.0 advance

--- application/basic/views/layouts/main.php
+++ application/basic/views/layouts/main.php
@@ -36,7 +36,7 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     //['label' => 'Home', 'url' => ['/site/index']],
-                    //['label' => 'About', 'url' => ['/site/about']],
+                    ['label' => 'Inventory', 'url' => ['/site/about']],
                     //['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
