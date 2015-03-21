9e5bc934768b399042511722dfdbcdb0cb642042
Phil Harvey Burgos <phburgoss@gmail.com>
2015-03-21 15:47:03 +0800

    Modified site/index.php
    Modified home page remove intructions

--- application/advanced/backend/views/site/index.php
+++ application/advanced/backend/views/site/index.php
@@ -4,13 +4,13 @@
 $this->title = 'RBI Inventory System Home Page';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
-
+<!--
 <p><h3>Instructions:</h3></p>
     <p>Press Alt + C, to go to Customer Page<br></p>
     <p>Press Alt + S, to go to Suppliers Page<br></p>
     <p>Press Alt + P, to go to Product Inventory Page<br></p>
     <p>Press Alt + O, to go to Order Page<br></p>
-    <p>Press Alt + T, to go to Transactions Page<br></p>
+    <p>Press Alt + T, to go to Transactions Page<br></p>-->
 
 <div class="site-index">
    
@@ -30,9 +30,6 @@ $this->title = 'RBI Inventory System Home Page';
      <div>
         <a href="index.php?r=order" title="Orders" accesskey="o"><img src="images/Orders2.png" height="200" width="200"><h2>Orders</h2></a>
         <br><br><br>
-    </div>      
-    <div>
-        <a href="index.php?r=transaction" title="Transactions" accesskey="t"><img src="images/Transactions2.png" height="200" width="200"><h2>Transactions</h2></a>
-    </div>     
+    </div>   
     </center>
 </div>
