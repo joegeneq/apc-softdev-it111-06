fa72786d8a6019df42012e2d09a24794bc9efe1e
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-03-29 00:52:41 +0800

    Updated UI and Shortcut keys for manage-customer/update.php and manage-customer/view.php

--- application/advanced/backend/views/manage-customer/view.php
+++ application/advanced/backend/views/manage-customer/view.php
@@ -7,15 +7,15 @@ use yii\widgets\DetailView;
 /* @var $model app\models\manageCustomer */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Manage Customers', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = ['label' => 'Manage Customers', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="manage-customer-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary','accesskey'=> 'u']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
@@ -44,5 +44,5 @@ $this->params['breadcrumbs'][] = $this->title;
             'updated_by',
         ],
     ]) ?>
-<?= Html::a('Back to Manage Customers', ['manage-customer/index'], ['class' => 'btn btn-primary']) ?>
+<?= Html::a('Back to Manage Customers', ['manage-customer/index'], ['class' => 'btn btn-primary','accesskey'=> 'b']) ?>
 </div>
