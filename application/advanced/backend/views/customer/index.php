6587ca9aa339111893cb06c83ee3ffb141d848cb
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-03-30 01:30:44 +0800

    Updated customer/index.php

--- application/advanced/backend/views/customer/index.php
+++ application/advanced/backend/views/customer/index.php
@@ -26,7 +26,7 @@ $this->title = 'Customers Page';
         'dataProvider' => $dataProvider,
        // 'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+           // ['class' => 'yii\grid\SerialColumn'],
 
             //'id',
             [
