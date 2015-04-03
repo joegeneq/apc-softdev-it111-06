7ad07fde0521fb8260ef554ec4d2d0be5ed922f9
Phil Harvey Burgos <phburgoss@gmail.com>
2015-04-03 21:48:22 +0800

    Added CRUD Purchase

---
+++ application/advanced/backend/controllers/PurchaseController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use backend\models\Purchase;
+use backend\models\PurchaseSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * PurchaseController implements the CRUD actions for Purchase model.
+ */
+class PurchaseController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Purchase models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new PurchaseSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Purchase model.
+     * @param integer $id
+     * @param integer $productinventory_id
+     * @param integer $customer_id
+     * @return mixed
+     */
+    public function actionView($id, $productinventory_id, $customer_id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id, $productinventory_id, $customer_id),
+        ]);
+    }
+
+    /**
+     * Creates a new Purchase model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Purchase();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'productinventory_id' => $model->productinventory_id, 'customer_id' => $model->customer_id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Purchase model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @param integer $productinventory_id
+     * @param integer $customer_id
+     * @return mixed
+     */
+    public function actionUpdate($id, $productinventory_id, $customer_id)
+    {
+        $model = $this->findModel($id, $productinventory_id, $customer_id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'productinventory_id' => $model->productinventory_id, 'customer_id' => $model->customer_id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Purchase model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @param integer $productinventory_id
+     * @param integer $customer_id
+     * @return mixed
+     */
+    public function actionDelete($id, $productinventory_id, $customer_id)
+    {
+        $this->findModel($id, $productinventory_id, $customer_id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Purchase model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @param integer $productinventory_id
+     * @param integer $customer_id
+     * @return Purchase the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id, $productinventory_id, $customer_id)
+    {
+        if (($model = Purchase::findOne(['id' => $id, 'productinventory_id' => $productinventory_id, 'customer_id' => $customer_id])) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
---
+++ application/advanced/backend/models/Purchase.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "purchase".
+ *
+ * @property integer $id
+ * @property string $date
+ * @property string $status
+ * @property integer $num_items
+ * @property string $total_amount
+ * @property integer $productinventory_id
+ * @property integer $customer_id
+ *
+ * @property Productinventory $productinventory
+ * @property Customer $customer
+ */
+class Purchase extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'purchase';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['date'], 'safe'],
+            [['status', 'num_items', 'productinventory_id', 'customer_id'], 'required'],
+            [['num_items', 'productinventory_id', 'customer_id'], 'integer'],
+            [['total_amount'], 'number'],
+            [['status'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'date' => 'Date',
+            'status' => 'Status',
+            'num_items' => 'Num Items',
+            'total_amount' => 'Total Amount',
+            'productinventory_id' => 'Productinventory ID',
+            'customer_id' => 'Customer ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductinventory()
+    {
+        return $this->hasOne(Productinventory::className(), ['id' => 'productinventory_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCustomer()
+    {
+        return $this->hasOne(Customer::className(), ['id' => 'customer_id']);
+    }
+}
