b5e7f361da0f774492edae148b6f3564e410a4cb
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-04-03 20:54:15 +0800

    Updated config/main-local.php for new DB Connection

--- application/advanced/common/config/main-local.php
+++ application/advanced/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=rbi_test',
+            'dsn' => 'mysql:host=localhost;dbname=rbi_is',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
