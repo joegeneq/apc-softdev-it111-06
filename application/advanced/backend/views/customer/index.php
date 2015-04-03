9b86037a2effd3a50a95da9f39d13ae8a2d4a3fe
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-04-03 21:48:45 +0800

    Re-modeled & Re-CRUD for Customer

---
+++ application/advanced/backend/views/customer/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\CustomerSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Customers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="customer-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Customer', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'type',
+            'contact_no',
+            'house_no',
+            // 'street',
+            // 'city',
+            // 'zip_code',
+            // 'country',
+            // 'email:email',
+            // 'contact_person',
+            // 'create_date',
+            // 'update_date',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
