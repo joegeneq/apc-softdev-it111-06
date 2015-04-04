d3142c0b6b84062209709985b487653e1a02065a
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-04-04 23:18:59 +0800

    Updated UI in manage-customer/update.php

--- application/advanced/backend/views/manage-customer/update.php
+++ application/advanced/backend/views/manage-customer/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\manageCustomer */
 
-$this->title = 'Update Manage Customer: ' . ' ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Manage Customers', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Update Customer: ' . ' ' . $model->name;
+//$this->params['breadcrumbs'][] = ['label' => 'Manage Customers', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+//$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="manage-customer-update">
 
