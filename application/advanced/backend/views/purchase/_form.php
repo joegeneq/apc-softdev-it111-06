b03283fc7b199229620414899f60eadf5b7f5f42
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-04-04 13:45:26 +0800

    Updated purchase/_form.php

--- application/advanced/backend/views/purchase/_form.php
+++ application/advanced/backend/views/purchase/_form.php
@@ -2,6 +2,10 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\customer;
+use backend\models\productinventory
+
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Purchase */
@@ -12,20 +16,29 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'date')->textInput() ?>
+    <?= $form->field($model, 'customer_id')->dropdownList(
+        ArrayHelper::map(customer::find()->all(), 'id', 'name'),
+        ['prompt'=>'Select Customer Name']
+    ) ?>
 
-    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'productinventory_id')->dropdownList(
+        ArrayHelper::map(productinventory::find()->all(), 'id', 'name'),
+        ['prompt'=>'Select Product Name']
+    ) ?>
 
-    <?= $form->field($model, 'num_items')->textInput() ?>
+     <?= $form->field($model, 'num_items')->textInput() ?>
 
-    <?= $form->field($model, 'total_amount')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'productinventory_id')->textInput() ?>
-
-    <?= $form->field($model, 'customer_id')->textInput() ?>
+    <?= $form->field($model, 'status')->dropDownList(
+    array('Pending'=>'Pending','Confirmed'=>'Confirmed', 'Cancelled'=>'Cancelled', 'For Replacement'=>'For Replacement'), 
+    ['prompt'=>'Select Purchase Status']); ?>
+    
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <div>
+     <?= Html::a('Back to Purchases', ['purchase/index'], ['class' => 'btn btn-primary', 'accesskey'=>'b']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
