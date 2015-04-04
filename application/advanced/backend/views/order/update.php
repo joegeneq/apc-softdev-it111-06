f747e0b0dd230d8145cddf7a4d1e432e1b6f4fc2
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-04-04 19:01:59 +0800

    Updated UI for order/update.php

--- application/advanced/backend/views/order/update.php
+++ application/advanced/backend/views/order/update.php
@@ -6,9 +6,9 @@ use yii\helpers\Html;
 /* @var $model backend\models\Order */
 
 $this->title = 'Update Order: ' . ' ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id, 'supplier_id' => $model->supplier_id, 'productinventory_id' => $model->productinventory_id]];
-$this->params['breadcrumbs'][] = 'Update';
+//$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id, 'supplier_id' => $model->supplier_id, 'productinventory_id' => $model->productinventory_id]];
+//$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="order-update">
 
