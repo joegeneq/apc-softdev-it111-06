c4243ca35574c6239477dd083922de08cfc6c863
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-04-04 23:28:53 +0800

    Updated UI in supplier/update.php

--- application/advanced/backend/views/supplier/update.php
+++ application/advanced/backend/views/supplier/update.php
@@ -6,9 +6,9 @@ use yii\helpers\Html;
 /* @var $model backend\models\Supplier */
 
 $this->title = 'Update Supplier: ' . ' ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Suppliers', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+//$this->params['breadcrumbs'][] = ['label' => 'Suppliers', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+//$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="supplier-update">
 
