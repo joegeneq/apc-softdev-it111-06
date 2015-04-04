42b7bc8d83e52c316310484f8a4e18cdbe24f712
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-04-04 13:35:20 +0800

    Updated productinventory/_form.php

--- application/advanced/backend/views/productinventory/_form.php
+++ application/advanced/backend/views/productinventory/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\supplier
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Productinventory */
@@ -20,20 +22,23 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'items_sold')->textInput() ?>
 
-    <?= $form->field($model, 'items_remaining')->textInput() ?>
-
     <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'status')->dropDownList(
+    array('Available'=>'Available','Out-of-stock'=>'Out-of-stock'), 
+    ['prompt'=>'Select Product Status']); ?>
 
-    <?= $form->field($model, 'create_date')->textInput() ?>
-
-    <?= $form->field($model, 'update_date')->textInput() ?>
-
-    <?= $form->field($model, 'supplier_id')->textInput() ?>
+      <?= $form->field($model, 'supplier_id')->dropDownList(
+          ArrayHelper:: map(supplier::find()->all(), 'id', 'name'),
+          ['prompt'=>'Select Supplier'])
+     ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <div>
+    <?= Html::a('Back to Product Inventory', ['product-inventory/index'], ['class' => 'btn btn-primary','accesskey'=>'b']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
