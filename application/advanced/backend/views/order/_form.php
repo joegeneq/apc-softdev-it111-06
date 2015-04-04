880a2d84801a940c91ebd61305855fb0ba6eecbc
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-04-04 13:39:37 +0800

    Updated order/_form.php

--- application/advanced/backend/views/order/_form.php
+++ application/advanced/backend/views/order/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\supplier;
+use backend\models\productinventory
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Order */
@@ -10,18 +13,26 @@ use yii\widgets\ActiveForm;
 
 <div class="order-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'date')->textInput() ?>
+    <?php $form = ActiveForm::begin(); ?>	
 
     <?= $form->field($model, 'qty')->textInput() ?>
 
-    <?= $form->field($model, 'supplier_id')->textInput() ?>
+    <?= $form->field($model, 'supplier_id')->dropDownList(
+          ArrayHelper:: map(supplier::find()->all(), 'id', 'name'),
+          ['prompt'=>'Select Supplier'])
+     ?>
 
-    <?= $form->field($model, 'productinventory_id')->textInput() ?>
+    <?= $form->field($model, 'productinventory_id')->dropdownList(
+        ArrayHelper::map(productinventory::find()->all(), 'id', 'name'),
+        ['prompt'=>'Select Product Name']
+    ) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <div>
+    	 <?= Html::a('Back to Orders', ['order/index'], ['class' => 'btn btn-primary', 'accesskey'=>'b']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
