4fe75ab9ab92a157fafeefc15727d7194dce47dd
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-04-04 19:00:59 +0800

    Updated UI and Redirection for order/index.php

--- application/advanced/backend/views/order/index.php
+++ application/advanced/backend/views/order/index.php
@@ -8,31 +8,50 @@ use yii\grid\GridView;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Orders';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="order-index">
 
+    <center><p>
+        <?= Html::a('Add Order', ['create'], ['class' => 'btn btn-success','accesskey' => 'a']) ?>
+        <?= Html::a('Manage Orders', ['manage-order/index'], ['class' => 'btn btn-danger','accesskey' => 'm']) ?>
+    </p><br>
+
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Order', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
+    </center>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        //'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+           //['class' => 'yii\grid\SerialColumn'],
+             [
+            'attribute' => 'id',
+            'format' => 'raw',
+            'value'=>function ($data) {
+           return Html::a(Html::encode($data->id), array('view', 'id'=>$data->id, 'customer_id'=>$data->customer_id, 'productinventory_id'=>$data->productinventory_id));
+        },
+           ],
+            [
+            'attribute' => 'supplier_id',
+            'value' => 'supplier.name',
+            ],
+            [
+            'attribute' => 'productinventory_id',
+            'value' => 'productinventory.name',
+            ],
 
-            'id',
+            //'id',
             'date',
             'qty',
-            'supplier_id',
-            'productinventory_id',
+        
 
-            ['class' => 'yii\grid\ActionColumn'],
+            //['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 
+    <p align="right">
+        <?= Html::a('Generate Orders', ['generated-order/index'], ['class' => 'btn btn-success','accesskey' => 'g']) ?>
+        <?= Html::a('Back to Home', ['site/index'], ['class' => 'btn btn-primary','accesskey' => 'h']) ?>
+    </p>
 </div>
