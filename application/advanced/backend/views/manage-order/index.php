9efaba6eb7105e227f5c05eaa37ca8a7e760224c
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-04-04 22:56:19 +0800

    Updated UI and Redirection in manage-order/index.php

--- application/advanced/backend/views/manage-order/index.php
+++ application/advanced/backend/views/manage-order/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel backend\models\manageOrderSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Manage Orders';
+$this->title = 'Manage Orders Page';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="manage-order-index">
