bec0a58fbb04d8bec6aa3dd515517cdd207fa5e5
Clarence Carillaga <clarence.carillaga@gmail.com>
2015-04-04 17:51:50 +0800

    Updated UI for productinventory/update.php

--- application/advanced/backend/views/productinventory/update.php
+++ application/advanced/backend/views/productinventory/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Productinventory */
 
-$this->title = 'Update Productinventory: ' . ' ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Productinventories', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id, 'supplier_id' => $model->supplier_id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Update Product: ' . ' ' . $model->name;
+//$this->params['breadcrumbs'][] = ['label' => 'Productinventories', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id, 'supplier_id' => $model->supplier_id]];
+//$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="productinventory-update">
 
