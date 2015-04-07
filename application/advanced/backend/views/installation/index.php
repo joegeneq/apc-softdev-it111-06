140b7ebd25740a990fb3cb77fe8802c452805377
Phil Harvey Burgos <phburgoss@gmail.com>
2015-04-07 14:54:18 +0800

    Added Installation controller and index page Also Added 2 images for installation page

---
+++ application/advanced/backend/views/installation/index.php
@@ -0,0 +1,23 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>Installation</h1>
+
+<h3> Requirements: </h3>
+
+<p class="installation">
+    1. Browser (Recommended: Google Chrome) <p class="note">Note: On Mozilla Firefox some bugs are not fix specially the accesskey.</p>
+</p>
+
+<p class="installation">
+	2. Screen Reader (NVDA) <br />
+	<a href="http://www.nvaccess.org/download/" class="hyperlink"> * Click Here to Download NVDA</a>
+</p>
+
+<h3>How to Install NVDA </h3>
+
+<p>1. Click the Skip Donation to download the NVDA software and wait until its finish <p>
+	<img src="images/installation/ht1.png"></img>
+<p>2. Run the NVDA software to start installation <p>
+	<img src="images/installation/ht2.png"></img>
+<p>3. And wait until it finish installation <p>
