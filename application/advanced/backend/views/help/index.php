af1f316205917a573afd33c4699f51c2a94823f2
Phil Harvey Burgos <phburgoss@gmail.com>
2015-04-07 14:52:30 +0800

    Added Help/Guide Page and Controller

---
+++ application/advanced/backend/views/help/index.php
@@ -0,0 +1,23 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>Help</h1>
+
+<h3> General Access Keys: </h3>
+<p class="installation">
+  1. Press Alt + C to Go to Customer Page.<br />
+  2. Press Alt + S to Go to Supplier Page.<br />
+  3. Press Alt + I to Go to Product Inventory Page.<br />
+  4. Press Alt + O to Go to Order Page. <br />
+  5. Press Alt + P to Go to Purchases Page.<br />
+  6. Press Alt + G to Go to Guide Page.<br />
+  7. Press Alt + H to Go to Home Page.<br />
+  8. Press Alt + L to Logout<br />
+</p>
+
+<h3> On Page Access Keys: </h3>
+<p class="installation">
+  1. Press Alt + A to Add Customers, Orders, Suppliers, Purchases and Product.<br />
+  2. Press Alt + M to Manage Customer, Orders, Suppliers, Purchases and Product.<br />
+  3. Press Alt + B or Backspace to Go Back.<br />
+</p> 
