87757d07e962562e40b709acbcfcae36682c8eb9
Pauline Gabrielle De Ocampo <paulinegabrielledeocampo@gmail.com>
2015-04-16 00:22:37 +0800

    Updated manage-order/index.php and manage-order/view.php

--- application/advanced/backend/views/manage-order/view.php
+++ application/advanced/backend/views/manage-order/view.php
@@ -7,15 +7,15 @@ use yii\widgets\DetailView;
 /* @var $model backend\models\manageOrder */
 
 $this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Manage Orders', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = ['label' => 'Manage Orders', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="manage-order-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id, 'supplier_id' => $model->supplier_id, 'productinventory_id' => $model->productinventory_id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Update', ['update', 'id' => $model->id, 'supplier_id' => $model->productinventory_id], ['class' => 'btn btn-primary','accesskey'=>'u']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id, 'supplier_id' => $model->supplier_id, 'productinventory_id' => $model->productinventory_id], [
             'class' => 'btn btn-danger',
             'data' => [
@@ -31,10 +31,12 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'date',
             'qty',
-            'supplier_id',
-            'productinventory_id',
             'price',
+            'supplier.name',
+            'productinventory.name',
         ],
     ]) ?>
-
+    <p align="left">
+    <?= Html::a('Back to Manage Orders', ['manage-order/index'], ['class' => 'btn btn-primary','accesskey'=>'b']) ?>
+    </p>
 </div>
